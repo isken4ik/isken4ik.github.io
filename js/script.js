b74999485a48d15e8b4aa31f84f619c5b3538d86
Isken <kggisken4ik@gmail.com>
2022-10-06 16:08:58 +0600

    UPD

--- js/script.js
+++ js/script.js
@@ -80,5 +80,68 @@ $(document).ready(function(){
   validateForms('#consultation form');
   validateForms('#order form');
 
-  $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+
+
+
+  $('form').submit((e) => {
+    e.preventDefault();
+
+    if(!$(this).valid()){
+        return;
+    }
+
+
+    $.ajax({
+        type: "POST",
+        url: ("mailer/smart.php"),
+        data: $(this).serialize()
+    }).done(() => {
+        $(this).find("input").val("");
+
+
+
+        $('form').trigger('reset');
+    });
+    return false;
+  });
+
+
+
+    $(window).scroll(function(){
+
+        if($(this).scrollTop() > 1400) {
+            $('.pageup').fadeIn();
+        }
+        else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+  
+  new WOW().init();
+
 });
